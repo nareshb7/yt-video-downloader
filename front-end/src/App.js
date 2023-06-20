9fb35a6854a84626c38f548a46cba59ba1590b76
Naresh Baleboina <104125516+nareshb7@users.noreply.github.com>
2023-06-20 12:27:52 +0530

    Update App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,7 +3,7 @@ import YTmusicdownloader from './components/music/YTmusicdownloader'
 
 const App = () => {
   return (
-    <div style={{textAlign:'center', fontFamily='cursive'}}>
+    <div style={{textAlign:'center', fontFamily:'cursive'}}>
         <YTmusicdownloader />
         <footer > Devloped by Naresh Baleboina.</footer>
     </div>
